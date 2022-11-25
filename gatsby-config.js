4a607c7e6f89f4e5830970069aa1738bb35eda2d
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2022-11-25 10:20:10 +0100

    Changed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MTG: Commander`,
+    description: `Naar aanleiding van de langlopende opdracht voor het vak CMS Development`,
+    author: `@Alex-Van-Dijck`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
