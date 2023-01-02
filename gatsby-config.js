92914a66ccc2543bc2ce2ae042267342f1fe8df8
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2023-01-02 15:08:53 +0100

    Basic query working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: `MTG: Commander`,
-    description: `Naar aanleiding van de langlopende opdracht voor het vak CMS Development`,
-    author: `@Alex-Van-Dijck`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: "MTG: Commander",
+    description: "Commander page desc",
+    author: "@alexvandijck",
+    siteUrl: "https://gatsbystarterdefaultsource.gatsbyjs.io/",
   },
   plugins: [
-    `gatsby-plugin-image`,
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-wordpress",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        /*
+         * De volledige URL van je Headless WordPress site's GraphQL API.
+         * Voorbeeld : "https://www.example-site.com/graphql"
+         */
+        url: "http://mtg-commander.local/graphql",
+        schema:{
+          timeout: 5000000
+        }
+        
       },
     },
   ],
-}
+};
--- src/pages/commanders.js
+++ src/pages/commanders.js
@@ -3,6 +3,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
 import Seo from "../components/seo";
+import Layout from '../components/layout';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,57 +17,66 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CommandersPage = () => {
+const CommandersPage = ({data: {allWpCommander: {edges}}}) => {
   const classes = useStyles();
 
-  //todo: Fix query
-  const data = useStaticQuery(graphql`
-    query {
-      allFile(filter: { extension: { eq: "jpg" } }) {
-        edges {
-          node {
-            childImageSharp {
-              fluid {
-                src
-              }
-            }
+  return (
+
+    <Layout pageTitle="Artists of Inghelbrecht Agency">
+      {edges.map((item) => {
+        const commander = item.node.commanderMeta;
+        return <p key={item.node.id}>{commander.name},{commander.description} test</p>
+      })}
+    </Layout>
+
+    // <div className={classes.root}>
+    //   <Grid container spacing={3}>
+    //     {edges.map((edge) => {
+    //       const commander = edge.node.commanderMeta;
+    //       <Grid item xs={12} sm={6} md={4} key={commander.name} >
+    //         <Card className={classes.card}>
+    //           <CardActionArea>
+    //             <CardMedia
+    //               className={classes.media}
+    //               image={commander.art.sourceUrl}
+    //               title={commander.name}
+    //             />
+    //             <CardContent>
+    //               <Typography gutterBottom variant="h5" component="h2">
+    //                 {commander.name}
+    //               </Typography>
+    //               <Typography variant="body2" color="textSecondary" component="p">
+    //                 {commander.node.description}
+    //               </Typography>
+    //             </CardContent>
+    //           </CardActionArea>
+    //         </Card>
+    //       </Grid>
+    //   })}
+    //   </Grid>
+    // </div>
+  );
+};
+
+export const query = graphql`
+  query {
+    allWpCommander {
+      edges {
+        node {
+          id
+          commanderMeta {
             name
+            description
+            art {
+              sourceUrl
+            }
           }
         }
       }
     }
-  `);
+  }
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        {data.allFile.edges.map((edge) => (
-          <Grid item xs={12} sm={6} md={4} key={edge.node.name}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={edge.node.childImageSharp.fluid.src}
-                  title={edge.node.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {edge.node.name}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Mauris volutpat nisl at justo faucibus, eu egestas tellus
-                    aliquet. In hac habitasse platea dictumst.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  );
-};
+`
 
 export const Head = () => <Seo title="Commanders" />
 
